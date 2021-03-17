3ac1901be1b1bee57d95db280c874768a3b49725
Andrew Hunter <huntersoftwaredevelopment@gmail.com>
2021-03-16 22:42:57 -0400

    Hello World 🖐

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "huntersoftwaredevelopment",
+  },
+  plugins: [],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+const height100 = {
+    height: "100%",
+};
+
+// markup
+const IndexPage = () => {
+    return (
+        <>
+            <div className={"container"} style={height100}>
+                <div className={"row"} style={height100}>
+                    <div className={"col align-self-center"}>
+                        <h1>Hunter Software Development</h1>
+                        <h4>Boutique Web Development in Greenville, South Carolina 📍</h4>
+                        <p>Coming soon...</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default IndexPage;
