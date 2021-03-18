42eab4478780e2093eab4aed3f1fd0e1b26a0835
Andrew Hunter <huntersoftwaredevelopment@gmail.com>
2021-03-17 23:33:33 -0400

    Update Icon ✨

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
-  siteMetadata: {
-    title: "huntersoftwaredevelopment",
-  },
-  plugins: [],
+    siteMetadata: {
+        title: "huntersoftwaredevelopment",
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Hunter Software Development`,
+                short_name: `Hunter Software Development`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/images/favicon.png`,
+            },
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                precachePages: [`/`],
+            },
+        },
+    ],
 };
