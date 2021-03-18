f8027d18328b1b09f4da751c7885ef874814aa10
Andrew Hunter <huntersoftwaredevelopment@gmail.com>
2021-03-17 23:04:22 -0400

    Add Fade Animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import FadeIn from "../components/fadeIn";
 
 const height100 = {
     height: "100%",
@@ -7,17 +8,22 @@ const height100 = {
 // markup
 const IndexPage = () => {
     return (
-        <>
-            <div className={"container"} style={height100}>
-                <div className={"row"} style={height100}>
-                    <div className={"col align-self-center"}>
+        <div className={"container"} style={height100}>
+            <div className={"row"} style={height100}>
+                <div className={"col align-self-center"}>
+                    <FadeIn delay={0} duration={500}>
                         <h1>Hunter Software Development</h1>
-                        <h4>Boutique Web Development in Greenville, South Carolina <span role="img" aria-label="pin">📍</span></h4>
+                        <h4>
+                            Boutique Web Development in Greenville, South Carolina
+                            <span role="img" aria-label="pin">
+                                📍
+                            </span>
+                        </h4>
                         <p>Coming soon...</p>
-                    </div>
+                    </FadeIn>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
