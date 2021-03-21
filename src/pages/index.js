e914c30911e2a658b36794df7cd5538a1f4214ad
Andrew Hunter <huntersoftwaredevelopment@gmail.com>
2021-03-20 23:57:48 -0400

    Resize Footer Better 🧈

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,21 +33,21 @@ const IndexPage = () => {
             <footer className="footer py-2 bg-light fixed-bottom">
                 <div className="container">
                     <div className={"row"}>
-                        <div className={"col text-center"}>
+                        <div className={"col-xxl text-center"}>
                             <span className={"text-muted"}>© Hunter Software Development LLC</span>
                         </div>
 
-                        <div className={"col text-center text-muted"}>
+                        <div className={"col-xxl text-center text-muted"}>
                             <a className={"text-reset"} href="tel:+1-864-214-5880" target={"_blank"} rel={"noreferrer"}>
                                 864-214-5880
                             </a>
                         </div>
-                        <div className={"col text-center text-muted"}>
-                            <a className={"text-reset float-end"} href="mailto:huntersoftwaredevelopment@gmail.com" target={"_blank"} rel={"noreferrer"}>
+                        <div className={"col-xxl text-center text-muted"}>
+                            <a className={"text-reset"} href="mailto:huntersoftwaredevelopment@gmail.com" target={"_blank"} rel={"noreferrer"}>
                                 huntersoftwaredevelopment@gmail.com
                             </a>
                         </div>
-                        <div className={"col text-center"}>
+                        <div className={"col-xxl text-center"}>
                             <a href={"https://www.linkedin.com/company/hunter-software-development"} target={"_blank"} rel={"noreferrer"}>
                                 <StaticImage src={"../images/socialIcons/linkedin.png"} alt={"linkedin"} width={16} height={16} style={{ verticalAlign: "middle" }} />
                             </a>
