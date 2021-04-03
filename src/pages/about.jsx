4cfa6299dbcf75f13776781790963da96c299c5e
Andrew Hunter <huntersoftwaredevelopment@gmail.com>
2021-04-03 02:18:45 -0400

    Send Links to blanks 🔗

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import FadeIn from "../components/fadeIn";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
@@ -26,7 +25,15 @@ const IndexPage = ({ location }) => {
                     <p>I was raised in Charleston, graduated from Clemson, and have spent the last few years living and working in Greenville.</p>
                     <p>I founded Hunter Software Development on the premise of providing simple, affordable, high-performing, quality websites to small businesses and non-profits.</p>
                     <p>
-                        By using <a href="https://www.gatsbyjs.com/">Gatsby</a> and <a href="https://www.netlifycms.org/">Netlify CMS</a> we are able to provide quality without having to deal with the bloat that most WordPress sites come with. More on that{" "}
+                        By using{" "}
+                        <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
+                            Gatsby
+                        </a>{" "}
+                        and{" "}
+                        <a href="https://www.netlifycms.org/" target="_blank" rel="noreferrer">
+                            Netlify CMS
+                        </a>{" "}
+                        we are able to provide quality without having to deal with the bloat that most WordPress sites come with. More on that{" "}
                         <Link to="/why" className="disabled">
                             here
                         </Link>
