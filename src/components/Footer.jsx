50b568054c072c57373fc1063ed79777df55ad6c
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-01-02 14:33:19 -0500

    Add Status + Github Links 🔗

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 
 import linkedinLogo from "../images/socialIcons/linkedin.png";
+import githubLogo from "../images/socialIcons/github.png";
+
 
 const Footer = () => {
     return (
@@ -16,15 +17,23 @@ const Footer = () => {
                         <a className={"text-reset"} href="tel:+1-864-214-5880" target={"_blank"} rel={"noreferrer"}>
                             864-214-5880
                         </a>
+                        <span> | </span>
+                        <a href={"https://www.linkedin.com/company/hunter-software-development"} target={"_blank"} rel={"noreferrer"}>
+                            <img src={linkedinLogo} alt={"linkedin"} width={16} height={16} style={{ verticalAlign: "middle" }} />
+                        </a>
+                        <span> | </span>
+                        <a href={"https://github.com/Hunter-Software-Development"} target={"_blank"} rel={"noreferrer"}>
+                            <img src={githubLogo} alt={"github"} width={16} height={16} style={{ verticalAlign: "middle" }} />
+                        </a>
                     </div>
                     <div className={"col-xxl text-center text-muted"}>
                         <a className={"text-reset"} href="mailto:huntersoftwaredevelopment@gmail.com" target={"_blank"} rel={"noreferrer"}>
                             huntersoftwaredevelopment@gmail.com
                         </a>
                     </div>
-                    <div className={"col-xxl text-center"}>
-                        <a href={"https://www.linkedin.com/company/hunter-software-development"} target={"_blank"} rel={"noreferrer"}>
-                            <img src={linkedinLogo} alt={"linkedin"} width={16} height={16} style={{ verticalAlign: "middle" }} />
+                    <div className={"col-xxl text-center text-muted"}>
+                        <a className={"text-reset"} href={"https://status.huntersoftware.dev"} target={"_blank"} rel={"noreferrer"}>
+                            status.huntersoftware.dev
                         </a>
                     </div>
                 </div>
